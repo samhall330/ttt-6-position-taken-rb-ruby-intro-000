59ccad33f78487368b49c0e70e09bbcb3881092a
Sam Hall <mann.samantha11@gmail.com>
2020-10-21 19:03:18 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
-def position_taken?(board, index)
 
+def position_taken?(board, index)
 
   if board[index] == " " || board[index] == "" || board[index] == nil
     false
   else board[index] == "X" || board[index] == "O"
     true
-
   end
-=======
-def position_taken?
-end
-
-def position_taken?(board, index)
-
-if board[index] != " "
-  return true
->>>>>>> f2be1ea2f6c146b235c386fd9049686ef9fb1f16
 end
